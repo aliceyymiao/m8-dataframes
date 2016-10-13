71adb2116a4a2e214c09aa1eebfdf6b3bc8be873
Alice Miao <Alice@D-173-250-172-249.dhcp4.washington.edu>
2016-10-13 15:08:32 -0700

    m8 participation

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,36 +1,39 @@
 # Exercise 5: Gates Foundation Educational Grants
 
 # Read data into a variable called `grants` using the `read.csv` function
-
+grants <- read.csv("./data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at your data
-
+View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending <- grants$total_amount
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
-# Unfortunately, it will not be a vector by default, so you must transform it using the as.vector function
+org <- grants$organization
 
+# Unfortunately, it will not be a vector by default, so you must transform it using the as.vector function
+as.vector(org)
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
 # What was the mean grant value?
-
+mean.value <- mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+largest.grant <- which.max(grants$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+smallest.grant <- which.min(grants$total_amount)
 
 # Which organization received the largest grant?
-
-
+org.largest.grant <- grants[which.max(grants$total_amount),][['total_spending']]
+largest.recipient <- grants[which.max(grants$total_amount),][['']]
 # Which organization received the smallest grant?
-
+org.smallest.grant <- grants[which.min(grants$total_amount),][['organization']]
 
 # How many grants were awarded in 2010?
+grants.awarded <- length(grants$total_amount)
--- exercise-7/exercise.r
+++ exercise-7/exercise.r
@@ -1,10 +1,12 @@
 # Exercise 7: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-
+husky.game.2015 <- read.csv('./data/huskies_2015.csv')
 # Create a vector of the teams that the Huskies played against during that season
+husky.game.2015$opponent
 # Call this vector `not.huskies`. You'll need to convert this column to a vector
-
+not.huskies <- as.vector(husky.game.2015$opponent)
 # Create a vector of the their final scores for the games
+final.scores <- as.vector(husky.game.2015$opponent_score)
 # Call this variable `husky.scores`
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
